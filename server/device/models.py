e4eecc6c60685d2b3108a9c514a9481f9a3f0442
Taiwo Ogunola <taiwo.ogunola.dev@gmail.com>
2021-05-16 14:00:44 +0100

    feat: attach user table to device model

--- server/device/models.py
+++ server/device/models.py
@@ -1,10 +1,14 @@
 from django.db import models
+from django.contrib.auth import get_user_model
 
+
+User = get_user_model()
 class Device(models.Model):
     device = models.CharField(max_length=200)
     manufacturer = models.CharField(max_length=200)
     os = models.CharField(max_length=200)
-    last_checked_out_by = models.CharField(max_length=200, blank=True, null=True)
+    last_checked_out_by = models.ForeignKey(
+        User, on_delete=models.SET_NULL, blank=True, null=True)
     is_checked_out = models.BooleanField(default=False)
     last_checked_out_date = models.DateTimeField(max_length=200, null=True, blank=True)
 
